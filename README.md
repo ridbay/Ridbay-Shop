e01317c9ffa3e34a17ea2a93c69fd908c3794a50
Ridwan Balogun <balogunridwan@gmail.com>
2019-09-04 14:40:08 +0100

    homepage skeleton

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+## Project
+
+Ecommerce App with ReactJs
---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React,  {Component}  from 'react';
+import './App.css';
+import Homepage from './homepage.component'
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;

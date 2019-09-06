afb64b89cc1147cd3d8a178a8d7130f8c296549b
Ridwan <artoas@Artoass-MacBook.Dual-Band-Broadband-Modem>
2019-09-06 18:39:36 +0100

    add shop page & component, collection item and collection preview components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-import React, { Component } from 'react';
-import './App.css';
-import Homepage from './pages/homepage/homepage.component'
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Homepage />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/shop" component={ShopPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;

00ebd519e14df3a30614287e02846df608676e9d
Ridwan Balogun <balogunridwan@gmail.com>
2019-09-11 13:12:33 +0100

    added cart icon and cart dropdown components, implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/shopify.svg';
+
 import './header.styles.scss';
-import {auth} from '../../firebase/firebase.util';
 
-import {connect} from 'react-redux'
+import CardIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown//cart-dropdown.component';
+import { auth } from '../../firebase/firebase.util';
 
-const Header = ({currentUser}) => (
+import { connect } from 'react-redux'
+
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className='logo-container' to='/'>
-            <Logo className="logo" width='50px' height='39px'/>
+            <Logo className="logo" width='50px' height='39px' />
         </Link>
         <div className='options'>
 
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/contact'>CONTACT</Link>
-        {
-            currentUser ? 
-            <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
-            :
-            <Link className="option" to='/signin'>SIGN IN</Link>
-        }
+            {
+                currentUser ?
+                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                    :
+                    <Link className="option" to='/signin'>SIGN IN</Link>
+            }
+            <CardIcon />
         </div>
+        {
+            hidden ? (null): (<CartDropdown />)
+        }
+
     </div>
 )
 
-const mapStateToProps = state =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header);

a40640eaa64881c0450c5bcb8b1a1e7bcd7cdf00
Ridwan <balogunridwan@gmail.com>
2019-09-12 16:29:35 +0100

    upadted cart icon component to display the accumulated cart quantity value

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,15 +4,19 @@ import{ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
-
+const mapStateToProps = ({cart: {cartItems}}) => (
+    {
+        itemCount: cartItems.reduce((accumulatedQuanity, cartItem) => accumulatedQuanity + cartItem.quantity,0)
+    }
+)
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);

4280421eaf034024b0e32101991c6663aaa3d3e7
Ridwan Balogun <balogunridwan@gmail.com>
2019-09-16 15:26:18 +0100

    added collection overview component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,8 +6,8 @@ import {createStructuredSelector} from 'reselect'
 import {selectDirectorySections} from '../../redux/directory/directory.selectors'
 
 const Directory = ({sections}) => (
-  <div className="directory-menu">
-    {this.state.sections.map(({ title, imageUrl, id, size }) => (
+  <div className='directory-menu'>
+    {sections.map(({ title, imageUrl, id, size }) => (
       <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
     ))}
   </div>

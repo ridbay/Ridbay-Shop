baebf1261a7d2e38acd83dac33ef93a4b575a689
Ridwan <balogunridwan@gmail.com>
2019-09-19 13:46:00 +0100

    Had serious issues with the custom button using styled component, had to revert back to pure css

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import {CustomButtonContainer} from './custom-button.styles'
+import './custom-button.styles.scss';
 
-
-const CustomButton = ({children, ...props}) => (
-    <CustomButtonContainer {...props}>
+const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
+    <button className={` ${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} {...otherProps}>
         {children}
-    </CustomButtonContainer>
+    </button>
 )
 
 export default CustomButton;

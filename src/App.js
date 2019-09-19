7a4cd2e0ba30a4ba6401f6519715dd4edca29bea
Ridwan <balogunridwan@gmail.com>
2019-09-19 14:18:39 +0100

    removed functions to move shop data to firebase from the App.js component

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component'
 
 
-import {auth, createUserProfileDocument, addCollectionAndDocuments} from './firebase/firebase.util';
+import {auth, createUserProfileDocument} from './firebase/firebase.util';
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
 import {createStructuredSelector} from 'reselect';
-import {selectCollectionsForPreview} from './redux/shop/shop.selectors'
 
 
 class App extends React.Component {
@@ -21,7 +20,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser, collectionsArray} = this.props;
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
@@ -35,7 +34,6 @@ class App extends React.Component {
           });
         }
         setCurrentUser(userAuth);
-        addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title, items})));
     
     });
   }
@@ -75,7 +73,6 @@ setCurrentUser: user=> dispatch(setCurrentUser(user))
 });
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  collectionsArray : selectCollectionsForPreview
+  currentUser: selectCurrentUser
 })
 export default connect(mapStateToProps, mapDispatchToProps)(App);

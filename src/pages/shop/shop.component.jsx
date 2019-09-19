5ebc1002ac1eea6f0c24b456d1df36cd1b6b1d10
Ridwan <balogunridwan@gmail.com>
2019-09-19 14:55:14 +0100

    brought shop data to the app from firebase

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-import {Route} from 'react-router-dom'
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component'
+import { Route } from "react-router-dom";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
+import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.util'
 
-const ShopPage =({match})=>(
-        <div className="shop-page">
-            <Route exact path={`${match.path}`} component={CollectionsOverview} />
-            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-        </div>
+class ShopPage extends React.Component {
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount(){
+        const collectionRef = firestore.collection('collections');
+        collectionRef.onSnapshot(async snapshot => {
+        convertCollectionsSnapshotToMap(snapshot);
+        })
+    }
+  render() {
+      const {match} = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
     );
+  }
+}
 
 export default ShopPage;

0f9366948da161652a4b6f0483133b35b6f1879b
Ridwan Balogun <balogunridwan@gmail.com>
2019-09-25 11:59:54 +0100

    Refactor Google sign in into sagas

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {connect} from 'react-redux'
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 
 import {auth, signInWithGoogle} from '../../firebase/firebase.util';
+import {googleSignInStart} from '../../redux/user/user.actions'
 
 
 class SignIn extends React.Component {
@@ -30,6 +32,7 @@ class SignIn extends React.Component {
         this.setState({ [name]: value })
     }
     render() {
+        const {googleSignInStart} = this.props
         return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
@@ -54,7 +57,12 @@ class SignIn extends React.Component {
                     />
                     <div className='buttons'>
                         <CustomButton type='submit'>sign in</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>sign in with Google</CustomButton>
+                        <CustomButton 
+                        type='button' 
+                        onClick={googleSignInStart} 
+                        isGoogleSignIn>
+                            sign in with Google
+                        </CustomButton>
                     </div>
                     
                     
@@ -63,5 +71,7 @@ class SignIn extends React.Component {
         )
     }
 }
-
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart())
+})
+export default connect(null, mapDispatchToProps)(SignIn);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@ import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import {persistStore} from 'redux-persist';
-import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 
 import rootSaga from './root-saga';

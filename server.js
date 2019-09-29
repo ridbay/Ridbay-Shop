63496c9ecc48b2d10ad03debd79f3ba459f6a973
Ridwan Balogun <balogunridwan@gmail.com>
2019-09-29 06:54:01 +0100

    debugged

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,15 +16,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build/')));
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log("server running on port " + port);
+  console.log('Server running on port ' + port);
 });
 
 app.post('/payment', (req, res) => {

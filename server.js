b81fe91fa1e82d68a2d569f827384fdeff0b1335
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-04 22:44:18 +0100

    debugging

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(cors());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend/build')));
 
+  
+
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
   });

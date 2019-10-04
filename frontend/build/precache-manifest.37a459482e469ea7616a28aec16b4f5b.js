67c34faf8d026d00521041ac55ccf6e7a4882aff
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-04 22:48:43 +0100

    debugging

---
+++ frontend/build/precache-manifest.37a459482e469ea7616a28aec16b4f5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213eec1289d0286463d966575e5b0ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9747d247e318bbeb2c32",
+    "url": "/static/css/main.2dadd36f.chunk.css"
+  },
+  {
+    "revision": "8f959942945f28357265",
+    "url": "/static/js/2.aca24580.chunk.js"
+  },
+  {
+    "revision": "9747d247e318bbeb2c32",
+    "url": "/static/js/main.6fa16854.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "297b257eafe37fd560d3210c07c09cd9",
+    "url": "/static/media/shopify.297b257e.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);

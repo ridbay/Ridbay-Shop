6c40d9fb7e11d9c747456b388cb833b56b5b6533
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-06 16:36:47 +0100

    Made website mobile responsive

--- frontend/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ frontend/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,33 +1,39 @@
-import React from "react";
-import "./cart-dropdown.styles.scss";
-import { connect } from "react-redux";
-import { selectCartItems } from "../../redux/cart/cart.selectors";
-import CustomButton from "../custom-button/custom-button.component";
-import CartItem from "../cart-item/cart-item.component";
-import { toggleCartHidden } from "../../redux/cart/cart.actions";
-import { createStructuredSelector } from "reselect";
-import { withRouter } from "react-router-dom";
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
+
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
+
+import {
+  CartDropdownContainer,
+  CartDropdownButton,
+  EmptyMessageContainer,
+  CartItemsContainer
+} from './cart-dropdown.styles';
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {cartItems.length > 0 ? (
+  <CartDropdownContainer>
+    <CartItemsContainer>
+      {cartItems.length ? (
         cartItems.map(cartItem => (
           <CartItem key={cartItem.id} item={cartItem} />
         ))
       ) : (
-        <span className="empty-message"> YOUR CART IS EMPTY</span>
+        <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
       )}
-    </div>
-    <CustomButton
+    </CartItemsContainer>
+    <CartDropdownButton
       onClick={() => {
-        history.push("/checkout");
+        history.push('/checkout');
         dispatch(toggleCartHidden());
       }}
     >
       GO TO CHECKOUT
-    </CustomButton>
-  </div>
+    </CartDropdownButton>
+  </CartDropdownContainer>
 );
 
 const mapStateToProps = createStructuredSelector({

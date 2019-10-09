d794884a18791a5e9a0e4772f05f5d73e88130af
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-09 14:27:34 +0100

    added lazy loading to all pages in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,7 @@ import { connect } from 'react-redux';
 
 import { createStructuredSelector } from 'reselect';
 
-import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component'
 
 
 import { selectCurrentUser } from './redux/user/user.selectors';
@@ -18,6 +15,9 @@ import { checkUserSession } from './redux/user/user.actions';
 import { GlobalStyle } from './global.styles';
 
 const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
+const ShopPage = lazy(()=> import("./pages/shop/shop.component"));
+const SignInAndSignUpPage = lazy(()=> import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const CheckoutPage = lazy(()=> import('./pages/checkout/checkout.component'));
 
 const App = ({ checkUserSession, currentUser }) => {
 

365635b0ec45d4d533e0f327cd2bdbe11f526b3e
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-11 06:23:23 +0100

    adding performance optimization to codebase

--- frontend/src/components/cart-item/cart-item.component.jsx
+++ frontend/src/components/cart-item/cart-item.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./cart-item.styles.scss";
 
+
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   <div className="cart-item">
     <img src={imageUrl} alt="item" />
@@ -10,5 +11,6 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     </div>
   </div>
 );
+
 
 export default React.memo(CartItem);
